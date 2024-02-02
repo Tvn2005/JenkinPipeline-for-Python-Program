5af184c01f635eaab6117b0082a8bfc78baf7801
Tejal Nijai <57373269+Tvn2005@users.noreply.github.com>
2024-02-02 12:50:47 +0000

    Create List_prg.py

---
+++ List_prg.py
@@ -0,0 +1,15 @@
+import os
+l1=[8,9,3,6,1,10]
+l1.reverse()
+print("The reversed list is",l1)
+
+l2=[91,67,120,34,76,54,76,87,56,64,345]
+l2.sort()
+print("The sorted list is",l2)
+
+l3=[]
+l3=l1.copy()
+print("List3=",l3)
+
+indexvalue=l2[2:6]
+print("the index values are",indexvalue)
